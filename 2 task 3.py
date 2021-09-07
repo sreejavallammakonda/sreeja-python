f75ff884c67592131f554185397ee5fa415a657c
sreejavallammakonda <90247593+sreejavallammakonda@users.noreply.github.com>
2021-09-07 14:23:45 +0530

    Add files via upload
    7-09-2021

---
+++ 2 task 3.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+c=int(input("Enter the value of c"))
+if(a>c):
+    
+    print("a is greater than c")
+elif(a>b):
+    print("a is greater than b")
+else:
+    print("b is greater than c")
